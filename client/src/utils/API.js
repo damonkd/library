1b42e16cda9e122d1cd46d04c9c8c9d1cdfa15ed
damonk@gmail.com <damonkd@gmail.com>
2019-07-15 23:52:12 -0700

    added google api route search for harry potter, not working currently

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -16,5 +16,11 @@ export default {
   // Saves a book to the database
   saveBook: function(bookData) {
     return axios.post("/api/books", bookData);
-  }
+  },
+  search: function() {
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q=harrypotter+intitle");
+  },
+
+
+
 };
